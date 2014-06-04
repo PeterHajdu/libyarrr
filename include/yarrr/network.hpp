abb2fcc7fcb075dea0e8242d006beba055a04d4c
Peter Ferenc Hajdu <peter.ferenc.hajdu@gmail.com>
2014-06-04 17:52:39 +0200

    split up SocketPool
     * Sockets will be responsible to handle an event on themselves
     * todo: drop connection

--- include/yarrr/network.hpp
+++ include/yarrr/network.hpp
@@ -1,15 +1,13 @@
 #pragma once
-//todo: remove iostream and inet.h
-#include <iostream>
-#include <arpa/inet.h>
-
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <cstring>
 #include <vector>
+#include <unordered_map>
 #include <poll.h>
 #include <algorithm>
+#include <memory>
 
 
 namespace yarrr
@@ -17,110 +15,77 @@ namespace yarrr
   class Socket
   {
     public:
+      typedef std::unique_ptr< Socket > pointer;
       Socket( int fd );
-      ~Socket();
-      const int fd;
+      virtual ~Socket();
 
-      void allow_address_reuse() const;
+      const int fd;
+      virtual void handle_event() = 0;
+  };
+
+  class ListeningSocket : public Socket
+  {
+    public:
+      typedef std::function<void(Socket::pointer&&)> AddSocketCallback;
+      ListeningSocket( int port, AddSocketCallback add_socket_callback );
+
+      virtual void handle_event() override;
+
+    private:
+      AddSocketCallback m_add_socket_callback;
   };
 
   template < typename MessageFactory >
   class SocketPool
   {
+    typedef SocketPool<MessageFactory> my_type;
     public:
       SocketPool()
-        : m_listeningSocket( socket( AF_INET, SOCK_STREAM, 0 ) )
       {
-        m_listeningSocket.allow_address_reuse();
       }
 
-      bool listen( int port )
+      void listen( int port )
       {
-        struct sockaddr_in address;
-        memset( &address, 0, sizeof( address ) );
-        address.sin_family = AF_INET;
-        address.sin_addr.s_addr = INADDR_ANY;
-        address.sin_port = htons( port );
+        add_socket( Socket::pointer(
+              new ListeningSocket(
+                port,
+                std::bind( &my_type::add_socket, this, std::placeholders::_1 ) ) ) );
+      }
 
-        if ( bind( m_listeningSocket.fd, (struct sockaddr *) &address, sizeof( address ) ) < 0 )
-        {
-          return false;
-        }
-
-        const int queue_length( 5 );
-        if ( ::listen( m_listeningSocket.fd, queue_length ) < 0 )
-        {
-          return false;
-        }
-
-        return true;
+      void add_socket( Socket::pointer&& socket )
+      {
+        m_poll_descriptors.emplace_back( pollfd{ socket->fd, POLLIN, 0 } );
+        m_sockets.emplace( std::make_pair( socket->fd, std::move( socket ) ) );
       }
 
       void start()
       {
-        m_fds.emplace_back(pollfd{ m_listeningSocket.fd, POLLIN, 0 });
         while ( true )
         {
           const int no_timeout( 0 );
-          if ( poll( &m_fds[0], m_fds.size(), no_timeout ) < 0 )
+          if ( poll( &m_poll_descriptors[0], m_poll_descriptors.size(), no_timeout ) < 0 )
           {
             //todo: handle network error
             return;
           }
 
-          handle_incoming_connection();
-          handle_incoming_user_data();
+          for ( auto& poll_descriptor : m_poll_descriptors )
+          {
+            const bool no_event( !poll_descriptor.revents & POLLIN );
+            if ( no_event )
+            {
+              continue;
+            }
+            m_sockets[ poll_descriptor.fd ]->handle_event();
+          }
+
         }
       }
 
     private:
 
-      bool has_connections()
-      {
-        return m_fds.size() > 1;
-      }
-
-      void handle_incoming_user_data()
-      {
-        if ( !has_connections() )
-        {
-          return;
-        }
-
-        auto second_element( begin( m_fds ) + 1 );
-        std::for_each( second_element, end( m_fds ),
-            []( pollfd& poll_descriptor )
-            {
-              if ( poll_descriptor.revents == 0 )
-              {
-                return;
-              }
-
-              MessageFactory::received_data();
-            } );
-      }
-
-      void handle_incoming_connection()
-      {
-        if ( m_fds[0].revents == 0 )
-        {
-          return;
-        }
-
-        struct sockaddr_in address;
-        socklen_t sin_size = sizeof( address );
-
-        m_fds.emplace_back(pollfd{
-            accept(m_listeningSocket.fd, (struct sockaddr *) &address, &sin_size),
-            POLLIN,
-            0 });
-
-        //todo: remove stupid printout
-        std::cout << "connection from: " << inet_ntoa( address.sin_addr ) << std::endl;
-      }
-
-      std::vector<pollfd> m_fds;
-      const Socket m_listeningSocket;
+      std::vector<pollfd> m_poll_descriptors;
+      std::unordered_map<int, Socket::pointer> m_sockets;
   };
 }
 
--- src/network.cpp
+++ src/network.cpp
@@ -1,24 +1,101 @@
 #include <yarrr/network.hpp>
 #include <unistd.h>
+#include <array>
 
-namespace yarrr
+//todo: remove iostream and inet.h
+#include <iostream>
+#include <arpa/inet.h>
+
+namespace
 {
-  Socket::Socket( int fd )
-    : fd( fd )
-  {
-  }
-
-  void
-  Socket::allow_address_reuse() const
+  //todo: consider error handling of these system functions
+  void allow_address_reuse( int socket )
   {
     const int true_value( 1 );
     setsockopt(
-        fd,
+        socket,
         SOL_SOCKET,
         SO_REUSEADDR,
         &true_value,
         sizeof( true_value ) );
   }
+
+
+  void bind_to_port( int port, int socket )
+  {
+    struct sockaddr_in address;
+    memset( &address, 0, sizeof( address ) );
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons( port );
+
+    bind( socket, (struct sockaddr *) &address, sizeof( address ) );
+  }
+
+
+  void listen_with_queue_length( int queue_length, int socket )
+  {
+    ::listen( socket, queue_length );
+  }
+
+
+
+  class ReadingSocket : public yarrr::Socket
+  {
+    public:
+      ReadingSocket( int socket )
+        : Socket( socket )
+      {
+      }
+
+      void handle_event() override
+      {
+        const size_t size_of_message( read( fd, &m_buffer[0], max_message_size ) );
+        std::cout
+          << "read " << size_of_message << " bytes -> ";
+        std::copy(
+            begin( m_buffer ), end( m_buffer ),
+            std::ostream_iterator< char >( std::cout ) );
+
+      }
+
+    private:
+      static const int max_message_size = 1000;
+      std::array<char, max_message_size> m_buffer;
+  };
+}
+
+
+namespace yarrr
+{
+  ListeningSocket::ListeningSocket( int port, AddSocketCallback add_socket_callback )
+    : Socket( socket( AF_INET, SOCK_STREAM, 0 ) )
+    , m_add_socket_callback( add_socket_callback )
+  {
+    allow_address_reuse( fd );
+    bind_to_port( port, fd );
+    listen_with_queue_length( 5, fd );
+  }
+
+
+  void
+  ListeningSocket::handle_event()
+  {
+    struct sockaddr_in address;
+    socklen_t sin_size = sizeof( address );
+
+    std::cout << "connection from: " << inet_ntoa( address.sin_addr ) << std::endl;
+    Socket::pointer new_socket( new ReadingSocket(
+          accept(fd, (struct sockaddr *) &address, &sin_size) ) );
+    m_add_socket_callback( std::move( new_socket ) );
+  }
+
+
+  Socket::Socket( int fd )
+    : fd( fd )
+  {
+  }
+
 
   Socket::~Socket()
   {
