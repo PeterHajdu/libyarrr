8e85f0507140f06c2c3d51fa9aeb98faf6604d8a
Peter Ferenc Hajdu <peter.ferenc.hajdu@gmail.com>
2014-07-29 16:17:17 +0200

    move chat message implementation to source file

--- include/yarrr/chat_message.hpp
+++ include/yarrr/chat_message.hpp
@@ -13,39 +13,17 @@ class ChatMessage : public Event
   public:
     add_polymorphic_ctci( "yarrr_chat_message" );
 
-    ChatMessage();
-
-    ChatMessage( const std::string& message, const std::string& sender )
-      : m_message( message )
-      , m_sender( sender )
-    {
-    }
-
-    const std::string& message() const
-    {
-      return m_message;
-    }
-
-    const std::string& sender() const
-    {
-      return m_sender;
-    }
+    ChatMessage() = default;
+    ChatMessage( const std::string& message, const std::string& sender );
+    const std::string& message() const;
+    const std::string& sender() const;
 
   private:
+    virtual void do_serialize( Serializer& serializer ) const override;
+    virtual void do_deserialize( Deserializer& deserializer ) override;
+
     std::string m_message;
     std::string m_sender;
-
-    virtual void do_serialize( Serializer& serializer ) const override
-    {
-      serializer.push_back( m_message );
-      serializer.push_back( m_sender );
-    }
-
-    virtual void do_deserialize( Deserializer& deserializer ) override
-    {
-      m_message = deserializer.pop_front< std::string >();
-      m_sender = deserializer.pop_front< std::string >();
-    }
 };
 
 }
--- src/chat_message.cpp
+++ src/chat_message.cpp
@@ -6,7 +6,42 @@ namespace
   yarrr::AutoEventRegister<yarrr::ChatMessage> register_chat_message;
 }
 
-yarrr::ChatMessage::ChatMessage()
+
+namespace yarrr
 {
+
+ChatMessage::ChatMessage( const std::string& message, const std::string& sender )
+  : m_message( message )
+  , m_sender( sender )
+{
+}
+
+const std::string&
+ChatMessage::message() const
+{
+  return m_message;
+}
+
+const std::string&
+ChatMessage::sender() const
+{
+  return m_sender;
+}
+
+
+void
+ChatMessage::do_serialize( Serializer& serializer ) const
+{
+  serializer.push_back( m_message );
+  serializer.push_back( m_sender );
+}
+
+void
+ChatMessage::do_deserialize( Deserializer& deserializer )
+{
+  m_message = deserializer.pop_front< std::string >();
+  m_sender = deserializer.pop_front< std::string >();
+}
+
 }
 
