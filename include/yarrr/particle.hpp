8f51a265da650a31ca9ab67735be6dd9791b1b8e
Peter Ferenc Hajdu <peter.ferenc.hajdu@gmail.com>
2014-08-15 15:43:33 +0200

    add particle factory interface

--- include/yarrr/particle.hpp
+++ include/yarrr/particle.hpp
@@ -7,6 +7,7 @@
 namespace yarrr
 {
 
+
 class Particle : public GraphicalObject
 {
   public:
@@ -22,6 +23,16 @@ class Particle : public GraphicalObject
     uint64_t age() const;
     PhysicalParameters m_physical_parameters;
     const the::time::Time m_time_of_birth;
+};
+
+
+class ParticleFactory
+{
+  public:
+    add_ctci( "yarrr_particle_factory" );
+    virtual ~ParticleFactory() = default;
+
+    virtual Particle::Pointer create( const PhysicalParameters& ) = 0;
 };
 
 }
