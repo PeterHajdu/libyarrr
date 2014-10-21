b61b098ba5b7f64c969961e476a11945e7292df0
Peter Ferenc Hajdu <peter.ferenc.hajdu@gmail.com>
2014-10-21 10:31:54 +0200

    move lua engine from server

---
+++ src/object_factory.cpp
@@ -0,0 +1,65 @@
+#include <yarrr/object_factory.hpp>
+#include <yarrr/lua_engine.hpp>
+#include <yarrr/log.hpp>
+#include <thectci/service_registry.hpp>
+#include <yarrr/object_creator.hpp>
+
+namespace
+{
+
+void register_object_factory( const std::string& name, sol::function factory )
+{
+  the::ctci::service< yarrr::ObjectFactory >().register_creator(
+      name,
+      [ factory, name ]()
+      {
+        thelog( yarrr::log::info )( "Calling lua factory method:", name );
+        try
+        {
+          const auto an_object( factory.call<yarrr::Object*>() );
+          return an_object->clone();
+        }
+        catch( std::exception& e )
+        {
+          thelog( yarrr::log::info )( "Lua factory method failed:", name, e.what() );
+        }
+        return yarrr::create_ship();
+      } );
+  thelog( yarrr::log::info )( "Registered lua factory method for object type:", name );
+}
+
+the::ctci::AutoServiceRegister< yarrr::ObjectFactory, yarrr::ObjectFactory > auto_object_factory_register;
+yarrr::AutoLuaRegister function_register(
+    []( yarrr::Lua& lua )
+    {
+      lua.state().set_function( "register_object_factory", register_object_factory );
+    } );
+}
+
+namespace yarrr
+{
+
+yarrr::Object::Pointer
+ObjectFactory::create_a( const std::string& key )
+{
+  Creators::const_iterator creator( m_creators.find( key ) );
+  if ( creator == m_creators.end() )
+  {
+    thelog( yarrr::log::warning )( "Unknown object key requested: ", key );
+    return nullptr;
+  }
+
+  return m_creators[ key ]();
+}
+
+void
+ObjectFactory::register_creator(
+    const std::string& key,
+    Creator creator )
+{
+  m_creators[ key ] = creator;
+  thelog( yarrr::log::info )( "Registered creator for type: ", key );
+}
+
+}
+
