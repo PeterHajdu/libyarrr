7d4ae4d13935664b273bc6a22467b03bf2b68436
Peter Hajdu <peter.ferenc.hajdu@gmail.com>
2015-03-30 13:52:54 +0200

    add protocol definitions

---
+++ include/yarrr/protocol.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <string>
+
+namespace yarrr
+{
+
+class Protocol
+{
+  public:
+    const static std::string object_assigned;
+    const static std::string login_request;
+    const static std::string registration_request;
+};
+
+}
+
---
+++ src/protocol.cpp
@@ -0,0 +1,12 @@
+#include <yarrr/protocol.hpp>
+
+
+const std::string
+yarrr::Protocol::object_assigned( "object assigned" );
+
+const std::string
+yarrr::Protocol::login_request( "login request" );
+
+const std::string
+yarrr::Protocol::registration_request( "registration request" );
+
