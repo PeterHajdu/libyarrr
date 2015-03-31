ca5f71084b7fc3f8bd8d04f8d56f84502f598fde
Peter Hajdu <peter.ferenc.hajdu@gmail.com>
2015-03-31 15:09:02 +0200

    add some more protocol definitions

--- include/yarrr/protocol.hpp
+++ include/yarrr/protocol.hpp
@@ -11,6 +11,8 @@ class Protocol
     const static std::string object_assigned;
     const static std::string login_request;
     const static std::string registration_request;
+    const static std::string authentication_request;
+    const static std::string authentication_response;
 };
 
 }
--- src/protocol.cpp
+++ src/protocol.cpp
@@ -10,3 +10,9 @@ yarrr::Protocol::login_request( "login request" );
 const std::string
 yarrr::Protocol::registration_request( "registration request" );
 
+const std::string
+yarrr::Protocol::authentication_request( "authentication request" );
+
+const std::string
+yarrr::Protocol::authentication_response( "authentication response" );
+
